f64cf3c6d7b5b36e8f1fffd248b65585e1500143
tian <istiancz@gmail.com>
2024-10-01 15:28:28 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import "./App.css";
 
 function App() {
   const [msgs, setMsgs] = useState<string[]>([]);
+  const [sending, setSending] = useState(false);
   return (
-    <div className="sm:mx-20 border p-4 rounded text-xl">
-      <ul className="messages text-right text-white">
+    <div className="sm:mx-20 border p-4 rounded text-xl h-screen overflow-hidden">
+      <ul className="messages text-right text-white pb-10 h-screen overflow-y-scroll ">
         {msgs.map((msg, i) => (
           <li
             className="ml-auto block w-fit mb-2 bg-blue-400 rounded-full px-4"
@@ -17,14 +18,21 @@ function App() {
       </ul>
       <form
         onSubmit={(e) => {
+          setSending(true);
           e.preventDefault();
 
+          changeY();
           const value = (e.target as any)[0].value;
           if (!value) return;
 
           setMsgs([...msgs, value]);
+          setTimeout(() => {
+            setSending(false);
+          }, 500);
         }}
-        className="border mt-4 flex"
+        className={`border mt-4 flex left-0 fixed bottom-2 w-full rounded-full ${
+          sending && "-z-10"
+        }`}
       >
         <input className="w-full" name="message" placeholder="Type a message" />
         <button className="border bg-blue-400 px-4 rounded-full" type="submit">
@@ -34,5 +42,16 @@ function App() {
     </div>
   );
 }
+
+const changeY = () => {
+  const eles = document.getElementsByTagName("li");
+  const ele = eles[eles.length - 1];
+  const windowHeight = window.innerHeight;
+  const height = ele?.getBoundingClientRect().top || 0;
+  const result = Math.max(windowHeight - height - 40, 0);
+  console.log({ height, windowHeight, result });
+  ele?.scrollIntoView({ behavior: "smooth" });
+  document.documentElement.style.setProperty("--y", `${result}px`);
+};
 
 export default App;
